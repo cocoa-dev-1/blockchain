0be5a84645c759319aac0e3e46b191ac39247760
cocoa-dev-1 <sinmounbu0510@naver.com>
2022-05-15 23:00:12 +0900

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,128 +1,56 @@
-type cocoa = {
-  <T>(a: T[]): void;
-};
+import crypto from 'crypto';
 
-const test: cocoa = (a) => {
-  console.log(a[0]);
-};
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-test([1, '1']);
-
-abstract class Class {
+class Block implements BlockShape {
+  public hash: string;
   constructor(
-    private id: number,
-    private count: number,
-    public name: string,
-    protected nickname: string
-  ) {}
-  getClassName() {
-    return this.name;
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
   }
-  getClassCount() {
-    return this.count;
-  }
-  abstract getCount(id: number): number;
-}
 
-class FirstClass extends Class {
-  getCount(id: number): number {
-    console.log(this.nickname);
-    return this.getClassCount();
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    const toHash: string = `${prevHash}${height}${data}`;
+    return crypto.createHash('sha256').update(toHash).digest('hex');
   }
 }
 
-const firstclass = new FirstClass(1, 22, '1반', '게임개발과');
-
-console.log(firstclass.getClassName());
-console.log(firstclass.getCount(1));
-console.log(firstclass.name);
-
-type Words = {
-  [key: string]: string;
-};
-
-class Dict {
-  private words: Words;
+class Blockchain {
+  private blocks: Block[];
   constructor() {
-    this.words = {};
+    this.blocks = [];
   }
-  add(word: Word): void {
-    if (this.words[word.term] == undefined) {
-      this.words[word.term] = word.def;
+  private getPrevHash(): string {
+    if (this.blocks.length === 0) {
+      return '';
     }
+    return this.blocks[this.blocks.length - 1].hash;
   }
-  def(term: string): string {
-    return this.words[term];
+  public addBlock(data: string): void {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
   }
-  update(term: string, def: string): boolean {
-    if (this.words[term] !== undefined) {
-      this.words[term] = def;
-      return true;
-    }
-    return false;
-  }
-  remove(term: string): boolean {
-    if (this.words[term] !== undefined) {
-      delete this.words[term];
-      return true;
-    }
-    return false;
+  public getBlocks() {
+    return [...this.blocks];
   }
 }
 
-class Word {
-  constructor(public readonly term: string, public readonly def: string) {}
-}
+const blockchain = new Blockchain();
 
-const kimchi = new Word('kimchi', '한국의 음식');
+blockchain.addBlock('First Block');
+blockchain.addBlock('Second Block');
+blockchain.addBlock('Third Block');
 
-const dict = new Dict();
-
-dict.add(kimchi);
-
-console.log(dict.def('kimchi'));
-
-type Job = 'IOT' | '인공지능' | '게임개발';
-
-interface User {
-  name: string;
-  job: Job;
-  nickname: string;
-  fullName(): string;
-}
-
-interface Human {
-  health: number;
-}
-
-class Player implements User, Human {
-  constructor(
-    public name: string,
-    public nickname: string,
-    public job: Job,
-    public health: number
-  ) {}
-  fullName(): string {
-    return `name: ${this.name}, nickname: ${this.nickname}`;
-  }
-}
-
-interface SStorage<T> {
-  [key: string]: T;
-}
-
-class LocalStorage<T> {
-  private storage: SStorage<T> = {};
-  set(key: string, value: T) {
-    this.storage[key] = value;
-  }
-  get(key: string): T {
-    return this.storage[key];
-  }
-  remove(key: string) {
-    delete this.storage[key];
-  }
-  clear() {
-    this.storage = {};
-  }
-}
+console.log(blockchain.getBlocks());
